a5bdf8cf2e43c28126009e3262d8c89fb7df349b
Shiho Kasai <shihokasai@ShihosMacBook-Pro.local>
2021-12-09 14:13:02 +0900

    JSの追加

---
+++ index.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const takanawa= document.getElementById("takanawa");
+
+takanawa.onclick = function() {
+  // takanawa.style.backgroundColor = "#08b3f0";
+  // takanawa.innerHTML = "クリックされた！";
+  takanawa.classList.add("blue");
+}
