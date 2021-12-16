3ccdac0dfa918678feffb9c030e5d3a27ad29e05
Shiho Kasai <shihokasai@ShihosMacBook-Pro.local>
2021-12-16 21:40:31 +0900

    JAVA追加

--- index.js
+++ index.js
@@ -1,9 +1,24 @@
 "use strict";
 
 const takanawa= document.getElementById("takanawa");
+const takawa=document.getElementById("takawa");
+const kouwa=document.getElementById("kouwa");
+const answerbox11= document.getElementById("answerbox11");
+const answerbox12=document.getElementById("answerbox12")
 
 takanawa.onclick = function() {
   // takanawa.style.backgroundColor = "#08b3f0";
   // takanawa.innerHTML = "クリックされた！";
   takanawa.classList.add("blue");
+  answerbox11.style.display="block";
+}
+
+takawa.onclick = function () {
+  takawa.classList.add("red")
+  answerbox12.style.display="block";
+}
+
+kouwa.onclick = function () {
+  kouwa.classList.add("red")
+  answerbox12.style.display="block";
 }
