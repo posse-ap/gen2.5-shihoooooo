826fa592b0a52753c47633297cf7c685ab7e9c17
Shiho Kasai <shihokasai@ShihosMacBook-Pro.local>
2022-09-23 09:22:10 +0900

    built in choices

--- src/quiz/index.php
+++ src/quiz/index.php
@@ -5,7 +5,20 @@ require("./dbconnect.php");
 $stmt = $pdo->prepare("SELECT * from big_questions"); //この時点で配列
 $stmt->execute();
 $big_questions = $stmt->fetchAll(PDO::FETCH_ASSOC); //$stmtに色々作業していく、最後にbigquestionsという変数にお全部入れる
-print_r($big_questions);
+// print_r($big_questions);
+
+$stmt = $pdo->prepare("SELECT * from questions where big_question_id=1"); //この時点で配列
+$stmt->execute();
+$questions = $stmt->fetchAll(PDO::FETCH_ASSOC);
+print_r($questions);
+
+
+$stmt = $pdo->prepare("SELECT * FROM choices INNER JOIN questions ON choices.question_id = questions.id where big_question_id=1"); 
+$stmt->execute();
+$choices = $stmt->fetchAll(PDO::FETCH_ASSOC);
+print('<pre>');
+print_r($choices);
+print('</pre>');
 
 // if (isset($_GET['id'])) {
 //   $id = $_GET['id'];
@@ -26,14 +39,24 @@ print_r($big_questions);
 </head>
 
 <body>
-  <?php echo $id; ?>
   <h1 id="title">ガチで東京の人しか解けない！ #<?php echo $big_questions[0]["name"]; //フロントに表示したいときはecho ?>  
   </h1>
   
 
   <section class="main" id="box">
+    <h2 class="quizbox" id="quiz${i}"><?php echo $questions[0]["id"]?>. この地名はなんて読む？</h2>
+      <div class="quiz-image-container">
+        <img class="quiz-image${i+1}" src="image/<?php echo $questions[0]["image"] ?>" alt="">
+      </div>
+      <ul id>
+        <?php foreach ($choices as $choice){ ?>
+           <li class="choice" id="1"> <?php echo $choice["name"]; ?></li>
+          <?php }; ?>
+      </ul>
+      </div>
   </section>
-  <script src="../index.js"></script>
+  <!-- <script src="../index.js"></script> -->
+  
 </body>
 
 </html>
